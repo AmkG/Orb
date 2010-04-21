9748da8b38dafe71e2dcfc16b3e7253d16373eab
Alan Manuel K. Gloria <almkglor@gmail.com>
2010-04-22 07:21:59 +0800

    Completed implementation of core thread-pool function

--- src/thread-pool.c
+++ src/thread-pool.c
@@ -10,7 +10,7 @@ static Orb_sema_t wait_sema;
  */
 struct list_s {
 	Orb_t val;
-	struct list_s* next;
+	struct list_s const* next;
 };
 typedef struct list_s list;
 typedef list const* list_t;
@@ -45,7 +45,7 @@ static queue_t queue_pop(queue_t q, Orb_t* prv) {
 		}
 		rv->insert_to = 0;
 		rv->remove_from = building->next;
-		*prv = building->Val;
+		*prv = building->val;
 		return rv;
 	} else {
 		rv->insert_to = q->insert_to;
@@ -138,11 +138,38 @@ retry:
 static Orb_t core_cfunc(Orb_t argv[], size_t* pargc, size_t argl) {
 	/*ignore all arguments*/
 	for(;;) {
-		/*pop off*/
+		tp_state* nv = Orb_gc_malloc(sizeof(tp_state));
+		Orb_t ostate = Orb_cell_get(state);
 		for(;;) {
-			/*TODO*/
+			tp_state_t curstate = Orb_t_as_pointer(ostate);
+			if(queue_empty(curstate->tasks)) {
+				/*attempt to increment waiters*/
+				nv->tasks = curstate->tasks;
+				nv->waiters = curstate->waiters + 1;
+				Orb_t readstate = Orb_cell_cas_get(state, ostate, Orb_t_from_pointer(nv));
+				if(readstate == ostate) {
+					/*update successful*/
+					Orb_sema_wait(wait_sema);
+					break;
+				} else ostate = readstate;
+			} else {
+				/*attempt to pop off*/
+				Orb_t todo;
+				queue_t nq = queue_pop(custate->tasks, &todo);
+				/*now try to update state*/
+				nv->tasks = nq;
+				nv->waiters = curstate->waiters;
+				Orb_t readstate = Orb_cell_cas_get(state, ostate, Orb_t_from_pointer(nv));
+				if(readstate == ostate) {
+					/*update successful*/
+					Orb_TRY {
+						Orb_call0(todo);
+					} Orb_CATCH(E) { /*do nothing*/ }
+					Orb_ENDTRY;
+					break;
+				} else ostate = readstate;
+			}
 		}
-		/*run*/
 	}
 }
 
