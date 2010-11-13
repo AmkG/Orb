0160b85599b5a325b0e04aba62a291e9fd148129
Alan Manuel K. Gloria <almkglor@mail.google.com>
2010-11-13 13:02:45 +0800

    Filled in red-black bs tree implementation

--- src/tree.c
+++ src/tree.c
@@ -93,9 +93,158 @@ static node_t ins(node_t n, Orb_tree_comparer_t cmp, Orb_t k, Orb_t v) {
 				n->c, balance(ins(n->l, cmp, k, v)),
 				n->k, n->v, n->r
 			);
+		} else if(cmp(nk, k)) {
+			return cnode(
+				n->c, n->l, n->k, n->v,
+				balance(ins(n->r, cmp, k, v))
+			);
 		} else {
-			/*TODO*/
+			return cnode(
+				n->c, n->l, k, v, n->r
+			);
 		}
 	}
+}
+
+/*Okasaki-style insertion*/
+static node_t cbalance(
+		node_t a,
+		Orb_t xk, Orb_t xv,
+		node_t b,
+		Orb_t yk, Orb_t yv,
+		node_t c,
+		Orb_t zk, Orb_t zv,
+		node_t d) {
+	return cnode(
+		color_red,
+		cnode(color_black,
+			a,
+			xk, xv,
+			b
+		),
+		yk, yv,
+		cnode(color_black,
+			c,
+			zk, zv,
+			d
+		)
+	);
+}
+
+static node_t balance(node_t n) {
+	if(n->c == color_red) return n;
+	/*no pattern matching, do it ourself!*/
+
+#define RET_CBAL \
+	return cbalance(a, xk, xv, b, yk, yv, c, zk, zv, d)
+
+	/* (T 'b a x (T 'r b y (T 'r c z d))) */
+	{
+		node_t a = n->l;
+		Orb_t xk = n->k, xv = n->v;
+		node_t tmp = n->r;
+
+		if(tmp == 0 || tmp->c == color_black) {
+			goto skip2; /*also skip the next pattern*/
+		}
+		node_t b = tmp->l;
+		Orb_t yk = tmp->k, yv = tmp->v;
+		node_t tmp2 = tmp->r;
+
+		if(tmp2 == 0 || tmp2->c == color_black) {
+			goto skip1;
+		}
+		node_t c = tmp2->l;
+		Orb_t zk = tmp2->k, zv = tmp2->v;
+		node_t d = tmp2->r;
+
+		RET_CBAL;
+	}
+skip1:
+
+	/* (T 'b a x (T 'r (T 'r b y c) z d)) */
+	{
+		node_t a = n->l;
+		Orb_t xk = n->k, xv = n->v;
+		node_t tmp = n->r;
+
+		/*check of tmp was already done before*/
+		node_t tmp2 = tmp->l;
+		Orb_t zk = tmp->k, zv = tmp->v;
+		node_t d = tmp->r;
+
+		if(tmp2 == 0 || tmp2->c == color_black) {
+			goto skip2;
+		}
+		node_t b = tmp2->l;
+		Orb_t yk = tmp2->k, yv = tmp2->v;
+		node_t c = tmp2->r;
+
+		RET_CBAL;
+	}
+skip2:
+
+	/* (T 'b (T 'r a x (T 'r b y c)) z d) */
+	{
+		node_t tmp = n->l;
+		Orb_t zk = n->k, zv = n->v;
+		node_t d = n->r;
+
+		if(tmp == 0 || tmp->c == color_black) {
+			goto skip4; /*also skip next pattern*/
+		}
+		node_t a = tmp->l;
+		Orb_t xk = tmp->k, xv = tmp->v;
+		node_t tmp2 = tmp->r;
+
+		if(tmp2 == 0 || tmp2->c == color_black) {
+			goto skip3;
+		}
+		node_t b = tmp2->l;
+		Orb_t yk = tmp2->k, yv = tmp2->v;
+		node_t c = tmp2->r;
+
+		RET_CBAL;
+	}
+skip3:
+
+	/* (T 'b (T 'r (T 'r a x b) y c) z d) */
+	{
+		node_t tmp = n->l;
+		Orb_t zk = n->k, zv = n->v;
+		node_t d = n->r;
+
+		/*tmp was already checked before*/
+		node_t tmp2 = tmp->l;
+		Orb_t yk = tmp->k, yv = tmp->v;
+		node_t c = tmp->r;
+
+		if(tmp2 == 0 || tmp2->c == color_black) {
+			goto skip4;
+		}
+		node_t a = tmp2->l;
+		Orb_t xk = tmp2->k, xv = tmp2->v;
+		node_t b = tmp2->r;
+
+		RET_CBAL;
+	}
+skip4:
+
+#undef RET_CBAL
+
+	return n;
+
+}
+
+/*insert routine*/
+Orb_tree_t Orb_tree_insert(Orb_tree_t tree, Orb_t k, Orb_t v) {
+	node_t nrv = ins(tree->top, tree->cmp, k, v);
+	nrv->c = color_black;
+
+	Orb_tree_t rv = Orb_gc_malloc(sizeof(Orb_tree));
+	rv->cmp = tree->cmp;
+	rv->top = nrv;
+
+	return rv;
 }
 
